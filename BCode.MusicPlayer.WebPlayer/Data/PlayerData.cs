14a98c3387bf1d310be6527be9ae4499c156e7da
B <B@MYMAINPC>
2024-04-28 10:52:26 +0100

    cleanup

--- BCode.MusicPlayer.WebPlayer/Data/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BCode.MusicPlayer.WebPlayer.Data
-{
-    public class PlayerData
-    {
-        public bool IsInitialized { get; set; }
-        public CancellationTokenSource CancelTokenSource { get; } = new CancellationTokenSource();
-        public string LastMessage { get; set; } = string.Empty;
-    }
-}
