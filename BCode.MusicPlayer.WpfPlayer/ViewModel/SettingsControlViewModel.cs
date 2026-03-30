3bd19aa7f5ce6b84cb67c3c03bc2763529137823
B <B@MYMAINPC>
2026-03-30 22:04:03 +0100

    track unsaved settings changes

--- BCode.MusicPlayer.WpfPlayer/ViewModel/SettingsControlViewModel.cs
+++ BCode.MusicPlayer.WpfPlayer/ViewModel/SettingsControlViewModel.cs
@@ -3,12 +3,18 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reactive.Linq;
 
 namespace BCode.MusicPlayer.WpfPlayer.ViewModel
 {
     public class SettingsControlViewModel : ReactiveObject
     {
         public ISettingsManager SettingsManager { get; set; }
+
+        private MusicPlayerSettings _originalSettings;
+
+        private bool _hasChanges;
+        public bool HasChanges { get => _hasChanges; set => this.RaiseAndSetIfChanged(ref _hasChanges, value); }
 
         public SettingsControlViewModel(ISettingsManager settingsManager)
         {
@@ -37,12 +43,26 @@ namespace BCode.MusicPlayer.WpfPlayer.ViewModel
                 current.UseCustomBackgroundImage = UseCustomBackgroundImage;
                 current.CustomBackgroundImagePath = CustomBackgroundImagePath;
                 SettingsManager.UpdateSettings(current);
+                // update original snapshot and clear dirty flag
+                _originalSettings = new MusicPlayerSettings
+                {
+                    LastVolume = current.LastVolume,
+                    UseCustomBackgroundImage = current.UseCustomBackgroundImage,
+                    CustomBackgroundImagePath = current.CustomBackgroundImagePath
+                };
+                HasChanges = false;
             }
         }
 
         public void Close()
         {
-            Save();
+            // Revert any unsaved changes by restoring values from the original snapshot
+            if (_originalSettings is not null)
+            {
+                UseCustomBackgroundImage = _originalSettings.UseCustomBackgroundImage;
+                CustomBackgroundImagePath = _originalSettings.CustomBackgroundImagePath;
+            }
+            HasChanges = false;
         }
 
         private void Init()
@@ -50,8 +70,47 @@ namespace BCode.MusicPlayer.WpfPlayer.ViewModel
             if (SettingsManager is not null)
             {
                 var current = SettingsManager.CurrentSettings;
-                UseCustomBackgroundImage = current.UseCustomBackgroundImage;
-                CustomBackgroundImagePath = current.CustomBackgroundImagePath;
+
+                // keep an original snapshot to detect changes and to allow revert
+                _originalSettings = new MusicPlayerSettings
+                {
+                    LastVolume = current.LastVolume,
+                    UseCustomBackgroundImage = current.UseCustomBackgroundImage,
+                    CustomBackgroundImagePath = current.CustomBackgroundImagePath
+                };
+
+                UseCustomBackgroundImage = _originalSettings.UseCustomBackgroundImage;
+                CustomBackgroundImagePath = _originalSettings.CustomBackgroundImagePath;
+
+                // subscribe to property changes to determine if we have unsaved changes
+                this.WhenAnyValue(x => x.UseCustomBackgroundImage, x => x.CustomBackgroundImagePath)
+                    .Subscribe(tuple =>
+                    {
+                        var (useCustom, path) = tuple;
+                        var temp = new MusicPlayerSettings
+                        {
+                            UseCustomBackgroundImage = useCustom,
+                            CustomBackgroundImagePath = path
+                        };
+
+                        HasChanges = !_originalSettings.Equals(temp);
+                    });
+
+                // listen for external settings changes and refresh snapshot
+                SettingsManager.SettingsChanged += (s, e) =>
+                {
+                    var updated = SettingsManager.CurrentSettings;
+                    _originalSettings = new MusicPlayerSettings
+                    {
+                        LastVolume = updated.LastVolume,
+                        UseCustomBackgroundImage = updated.UseCustomBackgroundImage,
+                        CustomBackgroundImagePath = updated.CustomBackgroundImagePath
+                    };
+
+                    UseCustomBackgroundImage = _originalSettings.UseCustomBackgroundImage;
+                    CustomBackgroundImagePath = _originalSettings.CustomBackgroundImagePath;
+                    HasChanges = false;
+                };
             }
         }
     }
